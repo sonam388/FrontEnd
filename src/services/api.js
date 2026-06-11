0f32997b652ca1408def7156281d6f1b35272ea1
Rishavchourasiya <rishavchourasiya945@gmail.com>
2026-06-11 11:01:27 +0530

    change api 2

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
- baseURL: "http://3.27.197.228:5000/api",
+ baseURL: "https://api.dayodaygaushalatendukheda.org/api",
   withCredentials: true,
 });
 
